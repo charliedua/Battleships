1039f042f849d8d635a78b88b39fe7350c6ebf25
Keito404 <saber_ali04@outlook.com>
2018-08-29 20:31:20 +1000

    Added namespaces to src and model files

---
+++ src/Direction.cs
@@ -0,0 +1,15 @@
+namespace MyGame
+{
+    public enum Direction
+    {
+        /// <summary>
+        ///     ''' The ship is oriented left/right
+        ///     ''' </summary>
+        LeftRight,
+
+        /// <summary>
+        ///     ''' The ship is oriented up/down
+        ///     ''' </summary>
+        UpDown
+    }
+}
